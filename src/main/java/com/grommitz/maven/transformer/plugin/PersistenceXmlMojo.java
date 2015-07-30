ef6395dd25e6774b32814b6ce3b5a524570654d3
Martin Charlesworth <martin.charlesworth@netnames.com>
2015-07-30 16:57:51 +0100

    outline mojo, doen't compile ATM

--- src/main/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojo.java
+++ src/main/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojo.java
@@ -1,5 +1,33 @@
 package com.grommitz.maven.transformer.plugin;
 
-public class PersistenceXmlMojo {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import com.google.common.collect.Lists;
+
+/**
+ * 
+ * @author Martin Charlesworth
+ *
+ */
+@Mojo(name = "persistencexml")
+public class PersistenceXmlMojo extends AbstractMojo {
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		List<Path> files = findPersistenceXmls();
+		files.stream().forEach(f -> System.out.println("Transforming " + f));
+		files.stream().forEach(f -> getLog().info("Transforming " + f));
+	}
+
+	private List<Path> findPersistenceXmls() {
+		return Lists.newArrayList(Paths.get("de2-core/src/main/resources/META-INF/persistence.xml"),
+				Paths.get("de2-reporting/src/main/resources/META-INF/persistence.xml"));
+	}
+	
 }
---
+++ src/test/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojoTest.java
@@ -0,0 +1,14 @@
+package com.grommitz.maven.transformer.plugin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenceXmlMojoTest {
+
+	@Test
+	public void testName() throws Exception {
+		
+	}
+	
+}
