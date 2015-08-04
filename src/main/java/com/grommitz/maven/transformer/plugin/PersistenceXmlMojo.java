3a83e5c33bb0d37c23857dbdf932e4cef81ea98c
Martin Charlesworth <martin.charlesworth@netnames.com>
2015-08-04 11:04:13 +0100

    added the code to find all the persistence xmls in the project's file tree

--- src/main/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojo.java
+++ src/main/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojo.java
@@ -1,7 +1,12 @@
 package com.grommitz.maven.transformer.plugin;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +14,14 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
+
+import com.google.common.collect.Lists;
 
 /**
+ * 
+ * Mojo which finds all persistence.xmls in the current project (and in modules if it
+ * is a multi-module project) and updates the version numbers of the jar dependencies in 
+ * them.
  * 
  * @author Martin Charlesworth
  *
@@ -19,17 +29,61 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 @Mojo(name = "persistencexml") //, defaultPhase = LifecyclePhase.INSTALL)
 public class PersistenceXmlMojo extends AbstractMojo {
 
+	private List<Path> persistenceXmls = new ArrayList<>();
+	private Path startingDir = Paths.get("").toAbsolutePath();
+	private final List<String> directoriesToExplore = 
+			Lists.newArrayList("src", "main", "test", "resources", "META-INF");
+
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		List<Path> files = findPersistenceXmls();
-		files.stream().forEach(f -> System.out.println("Transforming " + f));
-		files.stream().forEach(f -> getLog().info("Transforming " + f));
+		findPersistenceXmls();
+		persistenceXmls.stream().forEach(f -> getLog().info("Transforming " + f));
 	}
 
-	private List<Path> findPersistenceXmls() {
-		List<Path> results = new ArrayList<>();
-		results.add(Paths.get("de2-core/src/main/resources/META-INF/persistence.xml"));
-		results.add(Paths.get("de2-reporting/src/main/resources/META-INF/persistence.xml"));
-		return results;
+	public PersistenceXmlMojo startingDir(Path p) {
+		this.startingDir = p;
+		return this;
+	}
+	
+	public List<Path> getPersistenceXmls() {
+		return persistenceXmls;
+	}
+	
+	private void findPersistenceXmls() {
+		try {
+			Files.walkFileTree(startingDir, new PersistenceXmlFinder());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	class PersistenceXmlFinder extends SimpleFileVisitor<Path> {
+		
+		@Override
+		public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+			if (dir.equals(startingDir)) {
+				return FileVisitResult.CONTINUE;
+			}
+			if (dir.getParent().equals(startingDir)) {
+				return FileVisitResult.CONTINUE;
+			}
+			if (!Files.isDirectory(dir)) {
+				return FileVisitResult.CONTINUE;
+			}
+			String name = dir.getFileName().toString();
+			if (directoriesToExplore.contains(name)) {
+				return FileVisitResult.CONTINUE;
+			}
+			return FileVisitResult.SKIP_SUBTREE;
+		}
+		
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+			String name = file.getFileName().toString();
+			if (name.equals("persistence.xml")) {
+				persistenceXmls.add(file);
+			}
+			return FileVisitResult.CONTINUE;
+		}
 	}
 	
 }
--- src/test/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojoTest.java
+++ src/test/java/com/grommitz/maven/transformer/plugin/PersistenceXmlMojoTest.java
@@ -1,14 +1,62 @@
 package com.grommitz.maven.transformer.plugin;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * 
+ * @author Martin Charlesworth
+ *
+ */
 public class PersistenceXmlMojoTest {
 
-	@Test
-	public void testName() throws Exception {
-		
+	private Path startingDir;
+	
+	@Before
+	public void setUp() throws Exception {
+		setupTempDirectoryStructure();
 	}
 	
+	@After
+	public void tearDown() throws IOException {
+		FileUtils.deleteDirectory(startingDir.toString());
+	}
+	
+	@Test
+	public void should_find_persistence_xmls() throws Exception {
+		PersistenceXmlMojo mojo = new PersistenceXmlMojo().startingDir(startingDir);
+		mojo.execute();
+		assertThat(mojo.getPersistenceXmls().size(), is(2));
+	}
+	
+	private void setupTempDirectoryStructure() throws IOException {
+		Path tmp = Paths.get("/tmp");
+		startingDir = tmp.resolve("mojotest");
+		Files.createDirectories(startingDir);
+		createFilesUnder(startingDir);
+
+		Path module = startingDir.resolve("module");
+		Files.createDirectories(module);
+		createFilesUnder(module);
+	}
+	
+	private void createFilesUnder(Path projectRoot) throws IOException {
+		Path metaInf = projectRoot.resolve("src/main/resources/META-INF");
+		Files.createDirectories(metaInf);
+		Path xml = metaInf.resolve("persistence.xml");
+		Path txt = metaInf.resolve("persistence.txt");
+		if (!Files.exists(xml)) Files.createFile(xml);
+		if (!Files.exists(txt)) Files.createFile(txt);
+
+	}
 }
